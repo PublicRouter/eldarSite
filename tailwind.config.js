c16381e4409e42095a7323af2910d28d1f114764
publicrouter <jkggaringer@gmail.com>
2022-07-23 18:18:14 -0500

    edit response bg image size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,8 @@
 module.exports = {
   content: [
     "./src/App.js",
-    // "./src/components/CreateCharacter/CharacterForm.js",
-    "./src/components/CreateCharacter",
-    "./src/components/CharactersPage.js",
-    "./src/components/FightPage.js",
     "./src/components/Home.js",
-    "./src/components/Login.js",
-    "./src/components/Navigation.js",
-    "./src/components/CharactersList/index.js",
-    "./src/components/CharacterForm/index.js",
-    "./src/pages/CreateCharacterPage.js",
-    "./src/components/ReturnSignObject/index.js",
-    "./src/components/OrxCard/index.js",
-    "./src/pages/Profile.js",
-    "./src/pages/FightPage/index.js",
-    "./src/pages/FightPage/profile.js"
-
+    "./src/components/Navigation.js"
   ],
   theme: {
     extend: {
