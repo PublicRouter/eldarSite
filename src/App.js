fe449dfc62c7c229c6a4d488597ac4e10b670031
publicrouter <jkggaringer@gmail.com>
2022-08-16 15:52:15 -0500

    updating browser sizing for bg video

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className=''>
-      <video src={videoBg} autoPlay loop className='fixed top-0 left-0 w-full h-[100vh] object-cover z-[-10]'>
+      <video src={videoBg} muted autoPlay loop className='bgMain fixed top-0 left-0 w-full h-[100vh] object-cover z-[-10]'>
         {/* <source src="https://drive.google.com/file/d/114zQVHdvHKnK_1NJEf1ddlH4_rShxH6z/view?usp=sharing" /> */}
       </video>
       <Navigation />
