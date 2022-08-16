bbf265bbd936ebd9e4ed12cd7af6c2e5f41e0dd2
publicrouter <jkggaringer@gmail.com>
2022-08-16 15:27:33 -0500

    updating background image to video

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import videoBg from "./images/mainBg.mkv"
 
 import Home from './components/Home';
 import Navigation from "./components/Navigation"
@@ -9,11 +10,18 @@ function App() {
   
 
   return (
+    <div className=''>
+      <video src={videoBg} autoPlay loop className='fixed top-0 left-0 w-full h-[100vh] object-cover z-[-10]'>
+        {/* <source src="https://drive.google.com/file/d/114zQVHdvHKnK_1NJEf1ddlH4_rShxH6z/view?usp=sharing" /> */}
+      </video>
+      <Navigation />
+      <Home />
+    </div>
+      
 
-      <div className='leveled'>
-        <Navigation />
-        <Home />
-      </div>
+        
+        
+      
 
     
   );
